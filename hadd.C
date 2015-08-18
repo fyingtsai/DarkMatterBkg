7dec0bbb1b3d33f01febe4234b8858e3d40681f1
Fang-Ying Tsai <fyingtsai@gmail.com>
2015-08-18 19:11:20 +0800

    add a file hadd.C

---
+++ hadd.C
@@ -0,0 +1,156 @@
+#include <string.h>
+#include "TChain.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TTree.h"
+#include "TKey.h"
+#include "Riostream.h"
+
+TList *FileList;
+TFile *Target;
+
+void MergeRootfile( TDirectory *target, TList *sourcelist, double crossArray[] );
+
+
+void hadd() {
+
+  Target = TFile::Open( "Veto_DY.root", "RECREATE" );
+
+  FileList = new TList();
+
+  // ************************************************************
+ 
+  FileList->Add( TFile::Open("HT-100to200.root") ); 
+  FileList->Add( TFile::Open("HT-200to400.root") ); 
+  FileList->Add( TFile::Open("HT-400to600.root") ); 
+  FileList->Add( TFile::Open("HT-600toInf.root") ); 
+
+
+  // List of Cross-sections divided by num of events produced
+  double crossSections[4] = { 
+                139.4/2721604.,
+                42.75/973134.,
+                5.497/1049242.,
+                2.21/998912.};
+  // ************************************************************
+
+  MergeRootfile( Target, FileList, crossSections );
+
+}
+
+void MergeRootfile( TDirectory *target, TList *sourcelist, double crossArray[] ) {
+
+  TString path( (char*)strstr( target->GetPath(), ":" ) );
+  path.Remove( 0, 2 );
+
+  TFile *first_source = (TFile*)sourcelist->First();
+
+  first_source->cd( path );
+  TDirectory *current_sourcedir = gDirectory;
+  Bool_t status = TH1::AddDirectoryStatus();
+  TH1::AddDirectory(kFALSE);
+
+  // loop over all keys in this directory
+  TChain *globChain = 0;
+  TIter nextkey( current_sourcedir->GetListOfKeys() );
+  TKey *key, *oldkey=0;
+  while ( (key = (TKey*)nextkey())) {
+
+    //keep only the highest cycle number for each key
+    if (oldkey && !strcmp(oldkey->GetName(),key->GetName())) continue;
+
+    // read object from first source file
+    first_source->cd( path );
+    TObject *obj = key->ReadObj();
+
+    if ( obj->IsA()->InheritsFrom( "TH1" ) ) {
+      // descendant of TH1 -> merge it
+
+      cout << "Merging histogram " << obj->GetName() << endl;
+      TH1 *h1 = (TH1*)obj;
+
+      // Scale by the cross-section factor
+      h1->Scale(crossArray[0]);
+
+      // loop over all source files and add the content of the
+      // correspondant histogram to the one pointed to by "h1"
+      TFile *nextsource = (TFile*)sourcelist->After( first_source );
+
+      int q = 1; // This keeps track of which
+                 // cross section factor to use
+
+      while ( nextsource ) {
+        // make sure we are at the correct directory level by cd'ing to path
+        nextsource->cd( path );
+        TKey *key2 = (TKey*)gDirectory->GetListOfKeys()->FindObject(h1->GetName());
+        if (key2) {
+           TH1 *h2 = (TH1*)key2->ReadObj();
+
+       // Scale by the cross section factor
+           // before adding.
+           h2->Scale(crossArray[q]);
+           h1->Add( h2 );
+           q++;
+           delete h2;
+        }
+
+        nextsource = (TFile*)sourcelist->After( nextsource );
+      }
+    }
+    else if ( obj->IsA()->InheritsFrom( "TTree" ) ) {
+
+      // loop over all source files create a chain of Trees "globChain"
+      const char* obj_name= obj->GetName();
+
+      globChain = new TChain(obj_name);
+      globChain->Add(first_source->GetName());
+      TFile *nextsource = (TFile*)sourcelist->After( first_source );
+      //      const char* file_name = nextsource->GetName();
+      // cout << "file name  " << file_name << endl;
+     while ( nextsource ) {
+
+       globChain->Add(nextsource->GetName());
+       nextsource = (TFile*)sourcelist->After( nextsource );
+     }
+
+    } else if ( obj->IsA()->InheritsFrom( "TDirectory" ) ) {
+      // it's a subdirectory
+
+      cout << "Found subdirectory " << obj->GetName() << endl;
+
+      // create a new subdir of same name and title in the target file
+      target->cd();
+      TDirectory *newdir = target->mkdir( obj->GetName(), obj->GetTitle() );
+
+      // newdir is now the starting point of another round of merging
+      // newdir still knows its depth within the target file via
+      // GetPath(), so we can still figure out where we are in the recursion
+      MergeRootfile( newdir, sourcelist, crossArray);
+
+    } else {
+
+      // object is of no type that we know or can handle
+      cout << "Unknown object type, name: "
+           << obj->GetName() << " title: " << obj->GetTitle() << endl;
+    }
+
+    // now write the merged histogram (which is "in" obj) to the target file
+    // note that this will just store obj in the current directory level,
+    // which is not persistent until the complete directory itself is stored
+    // by "target->Write()" below
+    if ( obj ) {
+      target->cd();
+
+      //!!if the object is a tree, it is stored in globChain...
+    if(obj->IsA()->InheritsFrom( "TTree" ))
+          globChain->Merge(target->GetFile(),0,"keep");
+    else
+    obj->Write( key->GetName() );
+    }
+
+  } // while ( ( TKey *key = (TKey*)nextkey() ) )
+
+  // save modifications to target file
+  target->SaveSelf(kTRUE);
+  TH1::AddDirectory(status);
+}
