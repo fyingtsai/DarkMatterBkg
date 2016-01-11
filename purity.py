ef25d5855776acda7f2c6e93a73dd363aee1d94f
Fang-Ying Tsai <fyingtsai@gmail.com>
2016-01-11 14:11:22 +0800

    style setting

--- purity.py
+++ purity.py
@@ -1,45 +1,51 @@
 import ROOT
 from ROOT import TGraph, TFile, TCanvas
-from ROOT import gROOT
 from array import array
 
 def main():
-    c1 = TCanvas( 'c1', 'A Simple Graph', 200, 10, 700, 500 )
-    c1.SetGrid()
+    c1 = TCanvas("comp","",1100,1100)
+    pad = c1.cd()  
+    pad.SetGridx(1)
+    pad.SetGridy(1)
+
     f1 = TFile("GJ_out.root")
     f2 = TFile("QCD_out.root")
-    h_GJ = f1.Get("h_PtPho3")
-    h_QCD = f2.Get("h_PtPho3")
+    h_GJ = f1.Get("h_PtPho4")
+    h_QCD = f2.Get("h_PtPho4")
 
-    GJ_number = []
-    QCD_number = []
+    number_GJ = []
+    number_QCD = []
     ptValue = []
     purity = []
+
     for i in range(h_GJ.GetNbinsX()):
-        GJ_number.append(h_GJ.GetBinContent(i+1))
-        QCD_number.append(h_QCD.GetBinContent(i+1))
+        number_GJ.append(h_GJ.GetBinContent(i+1))
+        number_QCD.append(h_QCD.GetBinContent(i+1))
         ptValue.append((500/h_GJ.GetNbinsX())*i)
 
-    for j in range(len(GJ_number)):
-        if (GJ_number[j]+QCD_number[j]) == 0:
+    for j in range(len(number_GJ)):
+        if (number_GJ[j]+number_QCD[j]) == 0:
             purity.append(0)
         else:
-            purity.append(GJ_number[j]/(GJ_number[j]+QCD_number[j]))
+            purity.append(number_GJ[j]/(number_GJ[j]+number_QCD[j]))
 
     x = array("d", ptValue)
     y = array("d", purity)
 
-    fout = TFile("purity.root","recreate")
+    # fout = TFile("purity.root","recreate")
     g = TGraph(len(x),x,y)
     g.GetXaxis().SetTitle("Photon Pt (GeV)")
-    g.GetYaxis().SetTitle("Purity")
+    g.GetYaxis().SetTitle("Purity (S/S+B)")
     g.SetTitle("Photon Purity")
-    g.SetMarkerColor(4)
-    g.SetMarkerStyle(21)
-    g.Write('AP')
-    fout.Write()
-    fout.Close()
+    # g.SetMarkerColor(4)
+    g.SetMarkerStyle(20)
+    g.Draw('AP')
+    # fout.Write()
+    # fout.Close()
+    c1.Modified()
     c1.Update()
+    c1.cd()
+    c1.Print("graph1.png") 
 
 if __name__ == "__main__":
    main()
