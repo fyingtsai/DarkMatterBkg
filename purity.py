c1da4edd44a51e4913d947d03e3ffb1e2a990524
Fang-Ying Tsai <fyingtsai@gmail.com>
2016-01-12 14:45:30 +0800

    using TGraphAsymmErrors

--- purity.py
+++ purity.py
@@ -1,6 +1,6 @@
 import ROOT
 import math
-from ROOT import TGraph, TFile, TCanvas, TGraphErrors
+from ROOT import TGraph, TFile, TCanvas, TGraphAsymmErrors
 from array import array
 
 def main():
@@ -13,57 +13,18 @@ def main():
     f2 = TFile("QCD_out.root")
     h_GJ = f1.Get("h_PtPho4")
     h_QCD = f2.Get("h_PtPho4")
+    h_QCD.Add(h_GJ)
 
-    number_GJ = []
-    number_QCD = []
-    error_GJ = []
-    error_QCD = []
-    ptValue = []
-    purity = []
-    error = []
-    error_x = []
-
-    for i in range(h_GJ.GetNbinsX()):
-        number_GJ.append(h_GJ.GetBinContent(i+1))
-        number_QCD.append(h_QCD.GetBinContent(i+1))
-        error_GJ.append(h_GJ.GetBinError(i+1))
-        error_QCD.append(h_QCD.GetBinError(i+1))
-        ptValue.append((500/h_GJ.GetNbinsX())*i)
-
-    for j in range(len(number_GJ)):
-        if (number_GJ[j]+number_QCD[j]) == 0:
-            purity.append(0)
-        else:
-            purity.append(number_GJ[j]/(number_GJ[j]+number_QCD[j]))
-
-    for k in range(len(purity)):
-        if (number_GJ[k] and number_QCD[k]) == 0:
-            error.append(0)
-            error_x.append(0)
-        else:
-            error.append(purity[k]*math.sqrt(pow(error_GJ[k]/number_GJ[k],2)+pow(error_QCD[k]/number_QCD[k],2)))
-            error_x.append(0)
-
-    x = array("d", ptValue)
-    y = array("d", purity)
-    ey = array("d",error)
-    ex = array("d",error_x)
-
-
-    # fout = TFile("purity.root","recreate")
-    g = TGraphErrors(len(x),x,y,ex,ey)
+    g = TGraphAsymmErrors(h_GJ,h_QCD,"d")
     g.GetXaxis().SetTitle("Photon Pt (GeV)")
     g.GetYaxis().SetTitle("Purity (S/S+B)")
     g.SetTitle("Photon Purity")
-    # g.SetMarkerColor(4)
     g.SetMarkerStyle(20)
     g.Draw('AP')
-    # fout.Write()
-    # fout.Close()
     c1.Modified()
     c1.Update()
     c1.cd()
-    c1.Print("graph1.png") 
+    c1.Print("TGraphAsymmErrors.png") 
 
 if __name__ == "__main__":
    main()
